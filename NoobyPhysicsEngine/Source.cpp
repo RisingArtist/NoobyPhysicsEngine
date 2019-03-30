f787c5516dbf86b4a3625ead6f725b44215ec9e9
Andre Mercier <guardianandre4@gmail.com>
2019-03-30 16:30:52 -0400

    Add project files.

---
+++ NoobyPhysicsEngine/Source.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
+//Getting the AABB to work(Axis Aligned Bounding Box)
+//TODO: Getting the Bounding Circle implemented.
+//TODO: Implementing the SAT (Separating Axis Theorem)
+
+bool CheckAABBDetection(const RectangleShape& BoxA, const RectangleShape& BoxB);
+
+int main()
+{
+	//Created a 500 x 500 window
+	const int screenWidth = 500;
+	const int screenHeight = 500;
+	sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "AABB TEST");
+
+	//My Block that I would like to control (later)
+	sf::RectangleShape myBlock(Vector2f(30, 30));
+	myBlock.setFillColor(sf::Color::Magenta);
+	myBlock.setOrigin(myBlock.getLocalBounds().width / 2, myBlock.getLocalBounds().height / 2);
+	myBlock.setPosition(screenWidth / 2, screenHeight / 2);
+	//myBlock.rotate(45.0f);
+
+	//The block we should bump into
+	RectangleShape floor(Vector2f(100, 20));
+	floor.setFillColor(Color::Green);
+	floor.setOrigin(floor.getLocalBounds().width / 2, floor.getLocalBounds().height / 2);
+	floor.setPosition(screenWidth / 2, (screenHeight / 2) + 20);
+
+	//Game Loop
+	while (window.isOpen())
+	{
+		//Window events code only put in this loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		//Get Player's input and move myBlock in the proper direction
+#pragma region Input_Code
+		if (Keyboard::isKeyPressed(Keyboard::A))
+		{
+			myBlock.move(-1, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S))
+		{
+			myBlock.move(0, 1);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D))
+		{
+			myBlock.move(1, 0);
+		}
+		if (Keyboard::isKeyPressed(Keyboard::W))
+		{
+			myBlock.move(0, -1);
+		}
+#pragma endregion
+
+		//If they collide, turn blue
+		//otherwise, turn back to their original color
+		if (CheckAABBDetection(myBlock, floor))
+		{
+			myBlock.setFillColor(Color::Blue);
+			floor.setFillColor(Color::Blue);
+		}
+		else
+		{
+			myBlock.setFillColor(Color::Magenta);
+			floor.setFillColor(Color::Green);
+		}
+
+		//Refresh and Display the shapes
+		window.clear();
+		window.draw(floor);
+		window.draw(myBlock);
+		window.display();
+	}
+	window.close();
+	return 0;
+}
+
+bool CheckAABBDetection(const RectangleShape& BoxA, const RectangleShape& BoxB)
+{
+	//Get the distance
+	Vector2f distance = BoxA.getPosition() - BoxB.getPosition();
+
+	//Get the X Extents of the Boxes
+	float xExtent_A = BoxA.getSize().x / 2;
+	float xExtent_B = BoxB.getSize().x / 2;
+	float xOverlap = (xExtent_A + xExtent_B) - std::abs(distance.x);
+
+	if (xOverlap < 0.0) { return false; }
+
+	//Get the Y Extent of the Boxes
+	float yExtent_A = BoxA.getSize().y / 2;
+	float yExtent_B = BoxB.getSize().y / 2;
+
+	float yOverlap = (yExtent_A + yExtent_B) - std::abs(distance.y);
+
+	//Extra: Figure out which side of the box are we colliding in
+	if (yOverlap > xOverlap)
+	{
+		(distance.x > 0) ? std::cout << "Right Side\n" : std::cout << "Left Side\n";
+	}
+	else
+	{
+		(distance.y > 0) ? std::cout << "Bottom Side\n" : std::cout << "Top Side\n";
+	}
+
+	return (yOverlap > 0.0f) ? true : false;
+}
